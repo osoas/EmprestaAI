3eb13f4bb578442c7db730402fd97d513b0f07c8
osoas <isaiassilva1.com@gmail.com>
2025-10-29 08:02:50 -0300

    voltando pra versão anterior

--- src/main/java/br/edu/ifce/emprestaai/repository/UserRepository.java
+++ src/main/java/br/edu/ifce/emprestaai/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     //TODO -> adicionar outros find (find by email, find by name, etc)
+
 }
